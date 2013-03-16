0cc0d032833ba24c49b07922511539518cd5f70a
Joe Cross <joe.mcross@gmail.com>
2013-03-16 14:15:43 -0700

    Added dumb random method for SQLAlchemy models
    Not very intelligent error checking, random is on the fixed field id,
    assumes min value 1.

--- mtgquery/lib/alchemy_extensions.py
+++ mtgquery/lib/alchemy_extensions.py
@@ -1,3 +1,8 @@
+from sqlalchemy.sql import func
+import random
+random.seed(object())
+
+
 def get_or_create(session, model, **kwargs):
     '''
     Gets an object if it exists, otherwise returns a new model.
@@ -13,3 +18,31 @@ def has_model(session, model, **kwargs):
     (Really just an alias to the first function)
     '''
     return session.query(model).filter_by(**kwargs).first()
+
+
+def get_random(session, model):
+    '''
+    Returns a random object from a model based on the id field.
+    May take longer if rows have been deleted (will try to load non-existant rows)
+    Assumes ids start at 1.
+    '''
+    invalid_ids = []
+    max_id = session.query(func.max(model.id)).scalar()
+
+    def generate_id():
+        if len(invalid_ids) == max_id:
+            return None
+        while True:
+            id = random.randint(1, max_id)
+            if id not in invalid_ids:
+                # Can only be valid once, append
+                invalid_ids.append(id)
+                return id
+
+    while True:
+        id = generate_id()
+        if id is None:
+            return None
+        obj = has_model(session, model, id=id)
+        if obj is not None:
+            return obj
