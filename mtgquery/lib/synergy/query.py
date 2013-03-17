402d3b06d185bbc6e3c4b8321f5047e4aa14de84
Joe Cross <joe.mcross@gmail.com>
2013-03-17 01:30:46 -0700

    Revert hilarious debug change
    Previous commit fixed prod issue - weird that it wasn't a problem for me
    locally.
    Pulling out some debug code that never should have made it back in

--- mtgquery/lib/synergy/query.py
+++ mtgquery/lib/synergy/query.py
@@ -1,4 +1,3 @@
-from ..util import simple_timer
 from ...models import (
     DBSession,
     Synergy
@@ -13,13 +12,6 @@ def random_hash():
 def newest_synergies(n=10):
     data = []
     synergies = get_last_n(DBSession, Synergy, n, 'id', ['cards', 'texts'])
-    print '='*30
-    print '='*30
-    print "SHOULD BE DONE LOADING FROM DB"
-    print '='*30
-    print '='*30
-
-    t = simple_timer()
     for synergy in synergies:
         name = synergy.name if len(synergy.name) > 0 else "(Untitled)"
         data.append({
@@ -27,5 +19,4 @@ def newest_synergies(n=10):
             'length': len(synergy.cards) + len(synergy.texts),
             'url': '/s/{}'.format(synergy.hash)
         })
-    print "Loading synergy data took {} seconds".format(t())
     return data
