f0306e95eaf98874afadccac78473ddb3e65ac40
Joe Cross <joe.mcross@gmail.com>
2013-03-20 21:02:23 -0700

    refactor 2.2 migrate load_synergy
    Moved load_synergy into controllers/synergy

--- mtgquery/controllers/synergy.py
+++ mtgquery/controllers/synergy.py
@@ -1,6 +1,6 @@
-from mtgquery.lib.synergy.load_synergy import load_existing_synergy
 from mtgquery.lib.synergy.query import random_hash, newest_synergies
 from datetime import datetime
+from ..lib.parsers import card_group, synergy_group
 from ..lib.util.rate_limiting_generator import line_generator, rate_limit
 from ..lib.notifications import GenericNotification
 from ..lib.parsers import (
@@ -16,6 +16,15 @@ from ..models import (
     SynergyCard,
     SynergyText
 )
+
+
+class SynergyHashNotFoundException(Exception):
+    def __init__(self, hash):
+        self.hash = hash
+
+    @property
+    def msg(self):
+        return "Could not find the hash \"{}\"".format(self.hash)
 
 
 def create_synergy(cards, title, description):
@@ -64,7 +73,46 @@ def create_synergy(cards, title, description):
 
 
 def load_synergy(hash):
-    view_count, urls, counts, title, description, form_dict = load_existing_synergy(hash)
+    synergy = DBSession.query(Synergy).filter_by(hash=hash).first()
+    if synergy is None:
+        raise SynergyHashNotFoundException(hash)
+
+    #Update view count
+    synergy.view_count += 1
+    DBSession.add(synergy)
+
+    synergy_cards = synergy.cards
+    synergy_texts = synergy.texts
+
+    #Generate urls and counts
+    entries = []
+    url_base = 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid={mvid}&type=card'
+
+    for sc in synergy_cards:
+        url = url_base.format(mvid=sc.card.multiverse_id)
+        entries.append((sc.index, sc.quantity, url))
+    for st in synergy_texts:
+        url = 'http://res.cloudinary.com/mtg-query/image/upload/card_back.jpg'
+        text = st.text
+        url = text, url  # Packed data for synergy_raw
+        entries.append((st.index, st.quantity, url))
+    entries.sort()
+    counts = [e[1] for e in entries]
+    urls = [e[2] for e in entries]
+
+    title = synergy.title
+    description = synergy.description
+
+    card_list = card_group.to_list(synergy_cards)
+    text_list = synergy_group.to_list(synergy_texts)
+    lists = card_list + text_list
+    lists.sort()
+    form_cards_text = "\n".join([L[1] for L in lists])
+    form_dict = {
+        'form_cards_text': form_cards_text,
+        'form_title': title,
+        'form_description': description
+    }
     return urls, counts, title, description, form_dict
 
 
